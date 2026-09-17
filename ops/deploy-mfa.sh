6e3312529d34785ff937dfb70a5a5ef55ffb6dfa
Zursulus <105926475+Zursulus@users.noreply.github.com>
2026-09-17 16:45:03 +0300

    Fix MFA deployment requirements permissions

--- ops/deploy-mfa.sh
+++ ops/deploy-mfa.sh
@@ -29,6 +29,7 @@ pg_restore --list "$BACKUP/trud_site.dump" >/dev/null
 echo "Копия перед обновлением: $BACKUP"
 
 REQUIREMENTS=$(mktemp /tmp/trud-mfa-requirements.XXXXXXXX)
+chown trudsite:trudsite "$REQUIREMENTS"
 trap 'rm -f "$REQUIREMENTS"' EXIT
 gitapp show "$TARGET:backend/requirements.txt" > "$REQUIREMENTS"
 runuser -u trudsite -- "$APP/.venv/bin/pip" install --disable-pip-version-check -r "$REQUIREMENTS"
