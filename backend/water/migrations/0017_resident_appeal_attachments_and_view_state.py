fc6d6145dc09d6fd6b810d9f0550c29c5e2d022d
Zursulus <105926475+Zursulus@users.noreply.github.com>
2026-09-22 22:13:45 +0300

    ZUR-56: align appeal attachment migration with model defaults

--- backend/water/migrations/0017_resident_appeal_attachments_and_view_state.py
+++ backend/water/migrations/0017_resident_appeal_attachments_and_view_state.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import migrations, models
 import django.db.models.deletion
+import django.utils.timezone
 import water.resident_models
 
 
@@ -17,7 +18,7 @@ class Migration(migrations.Migration):
                 ('document', models.FileField(max_length=300, upload_to=water.resident_models.appeal_attachment_path, verbose_name='Файл')),
                 ('original_name', models.CharField(editable=False, max_length=255, verbose_name='Исходное имя')),
                 ('file_size', models.PositiveBigIntegerField(editable=False, verbose_name='Размер, байт')),
-                ('created_at', models.DateTimeField(editable=False, verbose_name='Загружено')),
+                ('created_at', models.DateTimeField(default=django.utils.timezone.now, editable=False, verbose_name='Загружено')),
                 ('appeal', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, related_name='attachments', to='water.residentappeal', verbose_name='Обращение')),
                 ('message', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.PROTECT, related_name='attachments', to='water.residentappealmessage', verbose_name='Сообщение жителя')),
                 ('uploaded_by', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, related_name='resident_appeal_attachments', to=settings.AUTH_USER_MODEL, verbose_name='Загрузил')),
